841187eadb4daf1dc3796374a956e8a2236498b9
Aleksander (asaikumaru) <asharovarin@gmail.com>
2024-01-26 01:24:03 +0300

    base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import svgrPlugin from 'vite-plugin-svgr'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  return {
+    plugins: [
+      react({
+        jsxImportSource: '@welldone-software/why-did-you-render', // <-----
+      }),
+      tsConfigPaths(),
+      svgrPlugin(),
+      // mkcert(),
+    ],
+    server: {
+      port: 5117,
+      // https: true,
+    },
+  }
 })
